fee681a249ac9026a731b2a09ea5b254a058e0ad
Checkmate128 <checkmate108@icloud.com>
2023-03-17 23:23:12 -0400

    Added UIScrollArea

--- src/test/OtherScreen.java
+++ src/test/OtherScreen.java
@@ -11,18 +11,18 @@ public class OtherScreen extends Screen {
         this.addComponent(new UIButton(40, 440, 400, 100, "Button", new ButtonStyle(), (button) -> {
             MainRender.switchScreen(new MainScreen());
         }));
-        this.addComponent(new UIButton(600, 440, 400, 100, "Button", false, new ButtonStyle(), (button) -> {
+        this.addComponent(new UIButton(600, 40, 400, 100, "Button", false, new ButtonStyle(), (button) -> {
             MainRender.switchScreen(new MainScreen());
         }));
         this.addComponent(new UIHorizontalSlider(300, 100, 500, 10, new Color(100, 0, 120), new Color(150, 90, 190), (slider) -> {
 
         }));
-        this.addComponent(new UIVerticalScrollBar(400, 160, 10, 400, new Color(220, 220, 220), new Color(140, 140, 140), 40));
+        this.addComponent(new UIVerticalScrollBar(400, 160, 10, 400, new ScrollBarStyle(), 40));
 
-        UICompoundComponent compoundTest = new UICompoundComponent(600, 600, 40, 40);
-        compoundTest.addComponent(new UIImageButton(4, 4, 120, 120, "/test/test_image.png", new Color(20, 20, 20, 50), (imageButton) -> {
+        UIScrollArea scrollTest = new UIScrollArea(600, 300, 400, 400, 800, 10, new ScrollBarStyle());
+        scrollTest.addComponent(new UIImageButton(4, 1080, 120, 120, "/test/test_image.png", new Color(20, 20, 20, 50), (imageButton) -> {
 
         }));
-        this.addComponent(compoundTest);
+        this.addComponent(scrollTest);
     }
 }
---
+++ src/test/ScrollBarStyle.java
@@ -0,0 +1,12 @@
+package test;
+
+import ui.Style;
+
+import java.awt.*;
+
+public class ScrollBarStyle extends Style {
+    public ScrollBarStyle() {
+        this.primaryColor = new Color(220, 220, 220);
+        this.secondaryColor = new Color(140, 140, 140);
+    }
+}
